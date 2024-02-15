3c3a129bcc2aa7876d23d177ded60754a9d7cf43
davit2kutelia <159577434+davit2kutelia@users.noreply.github.com>
2024-02-15 09:56:18 -0800

    Add files via upload

---
+++ GOA-homewrok/day 2/leasson1 .py
@@ -0,0 +1,16 @@
+name="data"
+surname="keshelava"
+print(name)
+name="data"
+age="14"
+height=183
+knows_programing=True
+
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programing))
+
+print(name + " " + str(age))
